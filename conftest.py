8910ad0f21bb1296a96efd44bcf5446cd2a0db13
saadira <rafsan.saadi@verizon.com>
2020-03-24 10:24:10 -0700

    initial project structure

---
+++ conftest.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from py.xml import html
+import pytest
+import logging
+
+logger = logging.getLogger(__name__)
+
+# #
+# #
+#   This to resolved module import handling path issue
+##
+##
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+# #
+# #
+#   Test run: pytest -s -v --html=reports/e2e_ps_test_report.html --self-contained-html tests/test_NsnApiSanity.py
+#   HTML Report Handler
+##
+##
+@pytest.mark.optionalhook
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.pop()
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.pop()
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_table_html(report, data):
+    if report.passed:
+        del data[:]
+        data.append(html.div('No log output captured.', class_='empty log'))
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    report.description = str(item.function.__doc__)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='marqeta_automation_demo',
+    version='1.0.0',
+    author='Rafsan Saadi',
+    author_email='rafsan.saadi@gmail.com',
+    packages=find_packages(),
+    description='Marqeta Automation Demo Test Framework',
+)
