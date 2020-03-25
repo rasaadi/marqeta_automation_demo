7d5590cc2a9699fc61d6603abd8b70b9a3c11564
saadira <rafsan.saadi@verizon.com>
2020-03-25 16:08:20 -0700

    1st step toward eleminating multiple obj creation

--- tests/test_card_creation.py
+++ tests/test_card_creation.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 class TestCardCreation(BaseTest):
     timestamp = UtilsHelper()
+    card_verifications = CardVerifications()
     user_dict = {
         "first_name": "Joe",
         "last_name": "Sam",
@@ -51,7 +52,7 @@ class TestCardCreation(BaseTest):
         return Data(user_token=user_client.user_token,
                     card_product_token=card_client.product_token)
 
-    #@pytest.mark.skip(reason="Test Disable")
+    # @pytest.mark.skip(reason="Test Disable")
     def test_create_card_success(self, resources):
         """
         Test create a new card  successfully
@@ -74,19 +75,19 @@ class TestCardCreation(BaseTest):
         #
         # ================ VERIFICATION ================
         #
-        card_verifications = CardVerifications()
-        card_verifications.verify_card_creation_success(card, resources)
+        # card_verifications = CardVerifications()
+        self.card_verifications.verify_card_creation_success(card, resources)
 
-    #@pytest.mark.skip(reason="Test Disable")
-    def test_create_card_without_user_token_fail(self, resources):
+    # @pytest.mark.skip(reason="Test Disable")
+    def test_create_multiple_cards_same_user_product_success(self, resources):
         """
-        Test create a new card without user token unsuccessfully
+        Test create multiple cards for same user and card product successfully
         """
         #
         # ================ CONFIGURATION ================
         #
         card_dict = {
-            "user_token": '',
+            "user_token": resources.user_token,
             "card_product_token": resources.card_product_token
         }
         card_details = json.dumps(card_dict)
@@ -95,43 +96,17 @@ class TestCardCreation(BaseTest):
         # ================ ACTION ================
         #
         card_client = CardActions()
-        card = card_client.create_card(card_details)
+        card1 = card_client.create_card(card_details)
+        card2 = card_client.create_card(card_details)
 
         #
         # ================ VERIFICATION ================
         #
-        card_verifications = CardVerifications()
-        card_verifications.verify_no_user_token_card_creation_fail(card)
+        # card_verifications = CardVerifications()
+        self.card_verifications\
+            .verify_multiple_cards_same_user_product_success(card1, card2)
 
-    #@pytest.mark.skip(reason="Test Disable")
-    def test_create_card_with_invalid_product_token_fail(self, resources):
-        """
-        Test create a new card  with invalid product token unsuccessfully
-        """
-        #
-        # ================ CONFIGURATION ================
-        #
-        card_dict = {
-            "user_token": resources.user_token,
-            "card_product_token": 'invalid_token'
-        }
-        card_details = json.dumps(card_dict)
-
-        #
-        # ================ ACTION ================
-        #
-        card_client = CardActions()
-        card = card_client.create_card(card_details)
-
-        #
-        # ================ VERIFICATION ================
-        #
-        logger.info("Card details: {}".format(card))
-        card_verifications = CardVerifications()
-        card_verifications.verify_invalid_product_token_card_creation_fail(
-            card)
-
-    #@pytest.mark.skip(reason="Test Disable")
+    # @pytest.mark.skip(reason="Test Disable")
     def test_create_personalized_card_with_name_success(self, resources):
         """
         Test create a new personalized card with custom name successfully
@@ -164,20 +139,20 @@ class TestCardCreation(BaseTest):
         #
         # ================ VERIFICATION ================
         #
-        card_verifications = CardVerifications()
-        card_verifications.verify_card_creation_custom_name_success(card,
+        # card_verifications = CardVerifications()
+        self.card_verifications.verify_card_creation_custom_name_success(card,
                                                                     custom_name)
 
     # @pytest.mark.skip(reason="Test Disable")
-    def test_create_multiple_cards_same_user_product_success(self, resources):
+    def test_create_card_without_user_token_fail(self, resources):
         """
-        Test create multiple cards for same user and card product successfully
+        Test create a new card without user token unsuccessfully
         """
         #
         # ================ CONFIGURATION ================
         #
         card_dict = {
-            "user_token": resources.user_token,
+            "user_token": '',
             "card_product_token": resources.card_product_token
         }
         card_details = json.dumps(card_dict)
@@ -186,12 +161,38 @@ class TestCardCreation(BaseTest):
         # ================ ACTION ================
         #
         card_client = CardActions()
-        card1 = card_client.create_card(card_details)
-        card2 = card_client.create_card(card_details)
+        card = card_client.create_card(card_details)
 
         #
         # ================ VERIFICATION ================
         #
-        card_verifications = CardVerifications()
-        card_verifications.verify_multiple_cards_same_user_product_success(
-            card1, card2)
+        # card_verifications = CardVerifications()
+        self.card_verifications.verify_no_user_token_card_creation_fail(card)
+
+    # @pytest.mark.skip(reason="Test Disable")
+    def test_create_card_with_invalid_product_token_fail(self, resources):
+        """
+        Test create a new card  with invalid product token unsuccessfully
+        """
+        #
+        # ================ CONFIGURATION ================
+        #
+        card_dict = {
+            "user_token": resources.user_token,
+            "card_product_token": 'invalid_token'
+        }
+        card_details = json.dumps(card_dict)
+
+        #
+        # ================ ACTION ================
+        #
+        card_client = CardActions()
+        card = card_client.create_card(card_details)
+
+        #
+        # ================ VERIFICATION ================
+        #
+        # card_verifications = CardVerifications()
+        self.card_verifications\
+            .verify_invalid_product_token_card_creation_fail(
+            card)
