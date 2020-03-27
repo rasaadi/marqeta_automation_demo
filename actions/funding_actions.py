87b6dcc61c3ec30db6d623febc2494b16fa708bc
saadira <rafsan.saadi@verizon.com>
2020-03-27 12:28:56 -0700

    added function description for funding action

--- actions/funding_actions.py
+++ actions/funding_actions.py
@@ -7,6 +7,9 @@ logger = logging.getLogger(__name__)
 
 class FundingActions(ApiGenericActions):
     def __init__(self):
+        """
+         init method to create object of this class
+         """
         super().__init__()
         self.funding_source_token = None
         self.funding_source_url = self.base_url + "fundingsources/program"
@@ -14,6 +17,11 @@ class FundingActions(ApiGenericActions):
         self.gpaorder_url = self.base_url + "gpaorders"
 
     def create_program_funding_source(self, source_details):
+        """
+        method to create program funding source
+        :param source_details: request payload containing program src details
+        :return: program_fund(json)
+        """
         logger.info("Creating a  program funding source")
 
         if source_details is not None:
@@ -21,22 +29,27 @@ class FundingActions(ApiGenericActions):
                                                 source_details)
 
             try:
-                new_program_fund = response_msg.json()
+                program_fund = response_msg.json()
             except ValueError as ve:
                 logger.error(
                     "Failed to convert funding source details into JSON")
 
             if http_code == 201:
-                self.funding_source_token = new_program_fund['token']
+                self.funding_source_token = program_fund['token']
             else:
                 logger.error("Error in the request, HTTP code: {}"
                              .format(http_code))
         else:
             logger.error("Missing card details")
 
-        return new_program_fund
+        return program_fund
 
     def create_gpaorder(self, gpaorder_payload):
+        """
+        method to create gaporder
+        :param gpaorder_payload: request payload containing gaporder details
+        :return: gpaorder(json)
+        """
         logger.info("Creating a new gpaorder")
 
         if gpaorder_payload is not None:
