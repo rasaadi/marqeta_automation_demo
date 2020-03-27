d4a442bf3b9dac1a0b71e8c658181056192d7eb4
saadira <rafsan.saadi@verizon.com>
2020-03-26 22:01:45 -0700

    saving test progress

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -10,8 +10,6 @@ from actions.payload_generator import PayloadGenerator
 from actions.transaction_actions import TransactionActions
 from actions.user_actions import UserActions
 from base.base_test import BaseTest
-from utils.utils_helper import UtilsHelper
-from verifications.card_verifications import CardVerifications
 from verifications.transaction_verifications import TransactionVerifications
 
 logger = logging.getLogger(__name__)
@@ -42,7 +40,7 @@ class TestTransaction(BaseTest):
 
         # Fund the user account
         funding_client.create_gpaorder(PayloadGenerator.gpaorder_payload(
-            user_token=user_client.user_token,
+            user_token=user_client.user_token, amount="100",
             funding_source_token=funding_client.funding_source_token))
 
         Data = namedtuple('Data',
@@ -59,7 +57,7 @@ class TestTransaction(BaseTest):
                     funding_source_token=funding_client.funding_source_token,
                     gpaorder_token=funding_client.gpaorder_token)
 
-    # @pytest.mark.skip(reason="Test Disable")
+    @pytest.mark.skip(reason="Test Disable")
     def test_create_transaction_success(self, resources):
         """
         Test create a new transaction  successfully
@@ -80,6 +78,44 @@ class TestTransaction(BaseTest):
         #
         # ================ VERIFICATION ================
         #
-        # logger.info(json.dumps(transaction, indent=2))
+        logger.info(json.dumps(transaction, indent=2))
         TransactionVerifications.verify_transaction_create_success(
             transaction, 15)
+
+    # @pytest.mark.skip(reason="Test Disable")
+    def test_transaction_decline_exceeds_fund_success(self, resources):
+        """
+        Test create a new transaction  successfully
+        """
+        #
+        # ================ CONFIGURATION ================
+        #
+        # Create user
+        resources.user_client.create_user(PayloadGenerator.user_payload())
+
+        # Create card product
+        resources.card_client.create_card_product(PayloadGenerator.
+                                                  card_product_payload())
+
+        # Create card for the user using card product
+        resources.card_client.create_card(PayloadGenerator.card_payload(
+            user_token=resources.user_client.user_token,
+            card_product_token=resources.card_client.product_token))
+
+        transaction_details = PayloadGenerator.transaction_payload(
+            amount="111", card_token=resources.card_client.card_token)
+
+        #
+        # ================ ACTION ================
+        #
+
+        transaction_actions = TransactionActions()
+        transaction = transaction_actions.create_transaction(
+            transaction_details)
+
+        #
+        # ================ VERIFICATION ================
+        #
+        logger.info(json.dumps(transaction, indent=2))
+        # TransactionVerifications.verify_transaction_create_success(
+        #     transaction, 15)
