58dbfb53d486334ffbadcdcba02d2f1a03d918ba
saadira <rafsan.saadi@verizon.com>
2020-03-27 22:08:20 -0700

    cleaned up last few unused code

--- base/static_config.py
+++ base/static_config.py
@@ -7,8 +7,8 @@ logger = logging.getLogger(__name__)
 class StaticConfig:
     # Sandbox Details:
     SANDBOX_BASE_URL = "https://sandbox-api.marqeta.com/v3/"
-    APP_TOKEN = '5b4be8a7-c908-4840-8c57-3b40253e7831'
-    MASTER_TOKEN = '8d810c08-4680-4e8b-bc73-f142bb605aa3'
+    APP_TOKEN = 'placeholder'
+    MASTER_TOKEN = 'placeholder'
 
     ''''
     Enable this block of the code if the project is intended to run in 
@@ -22,12 +22,3 @@ class StaticConfig:
     # except AttributeError as AE:
     #     logger.exception("Exception occurred while parsing Env variable: {0}"
     #                      .format(AE))
-
-    NEW_USER_EMAIL = "sam.smith@mailinator.com"
-
-    # List of PATHs
-    ABS_PATH_TO_BASE_TEST = os.path.dirname(os.path.abspath(__file__))
-    ABS_PATH_TO_ROOT = os.path.dirname(ABS_PATH_TO_BASE_TEST)
-
-    REPORT_PATH = os.path.join(ABS_PATH_TO_ROOT, "reports/")
-    RESOURCE_PATH = os.path.join(ABS_PATH_TO_ROOT, "resources/")
