a4f0e4c09484dec122ee565a84add8a196665d87
saadira <rafsan.saadi@verizon.com>
2020-03-27 20:32:14 -0700

    added env handler

--- base/static_config.py
+++ base/static_config.py
@@ -11,6 +11,19 @@ class StaticConfig:
     APP_TOKEN = '5b4be8a7-c908-4840-8c57-3b40253e7831'
     MASTER_TOKEN = '8d810c08-4680-4e8b-bc73-f142bb605aa3'
 
+    ''''
+    Enable this block of the code if the project is intended to run in 
+    Jenkins or use System Environment variable to parse APP_TOKEN and 
+    MASTER_TOKEN
+    '''
+    # try:
+    #     APP_TOKEN = str(os.getenv("APP_TOKEN")).strip()
+    #     MASTER_TOKEN = str(os.getenv("MASTER_TOKEN")).strip()
+    #
+    # except AttributeError as AE:
+    #     logger.exception("Exception occurred while parsing Env variable: {0}"
+    #                      .format(AE))
+
     NEW_USER_EMAIL = "sam.smith@mailinator.com"
 
     # System agnostic PATH info
