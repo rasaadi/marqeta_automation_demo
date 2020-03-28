e24ab91e9572d3ff9589e1fadb695a1cae70e531
saadira <rafsan.saadi@verizon.com>
2020-03-27 20:33:14 -0700

    reformat the code

--- base/static_config.py
+++ base/static_config.py
@@ -5,7 +5,6 @@ logger = logging.getLogger(__name__)
 
 
 class StaticConfig:
-
     # Sandbox Details:
     SANDBOX_BASE_URL = "https://sandbox-api.marqeta.com/v3/"
     APP_TOKEN = '5b4be8a7-c908-4840-8c57-3b40253e7831'
@@ -26,10 +25,9 @@ class StaticConfig:
 
     NEW_USER_EMAIL = "sam.smith@mailinator.com"
 
-    # System agnostic PATH info
+    # List of PATHs
     ABS_PATH_TO_BASE_TEST = os.path.dirname(os.path.abspath(__file__))
     ABS_PATH_TO_ROOT = os.path.dirname(ABS_PATH_TO_BASE_TEST)
 
     REPORT_PATH = os.path.join(ABS_PATH_TO_ROOT, "reports/")
     RESOURCE_PATH = os.path.join(ABS_PATH_TO_ROOT, "resources/")
-
