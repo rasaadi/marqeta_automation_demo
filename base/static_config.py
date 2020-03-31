f40701311d96bf6f1247db2805e6d1ae4d3c6938
saadira <rafsan.saadi@verizon.com>
2020-03-30 18:14:38 -0700

    added details desc for the use of APP and MASTER token

--- base/static_config.py
+++ base/static_config.py
@@ -7,11 +7,16 @@ logger = logging.getLogger(__name__)
 class StaticConfig:
     # Sandbox Details:
     SANDBOX_BASE_URL = "https://sandbox-api.marqeta.com/v3/"
+
+    '''
+    Enable/Disable this block of code if you want to run code directly(not 
+    to deal with env) with your own Application token and Master Token
+    '''
     # APP_TOKEN = 'placeholder'
     # MASTER_TOKEN = 'placeholder'
 
     ''''
-    Enable this block of the code if the project is intended to run in 
+    Enable/Disable this block of the code if the project is intended to run in 
     Jenkins or GitHub Workflow or use System Environment variable to parse 
     APP_TOKEN and 
     MASTER_TOKEN
